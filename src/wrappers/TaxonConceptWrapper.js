6bdccca184b311288bee32828431379203b7b37f
Gaurav Vaidya <gaurav@ggvaidya.com>
2019-06-10 19:00:25 -0400

    Renamed asEquivClass() to asOWLEquivClass() to be less ambiguous.

--- src/wrappers/TaxonConceptWrapper.js
+++ src/wrappers/TaxonConceptWrapper.js
@@ -154,14 +154,14 @@ class TaxonConceptWrapper {
    * Note that we don't include the accordingTo information in this
    * query, since we don't have a useful way to use that during OWL reasoning.
    */
-  asEquivClass() {
+  asOWLEquivClass() {
     // Without a taxonomicName, we can't do anything.
     if (!this.taxonName) return undefined;
 
     return {
       '@type': 'owl:Restriction',
       onProperty: 'hasName',
-      someValuesFrom: new TaxonNameWrapper(this.taxonName).asEquivClass(),
+      someValuesFrom: new TaxonNameWrapper(this.taxonName).asOWLEquivClass(),
     };
   }
 }
