4818ed38de7ba6b1badb3f36d73e99a138201262
Gaurav Vaidya <gaurav@ggvaidya.com>
2019-06-13 18:17:54 -0400

    Allow nomenclatural code to be set; made asJSONLD a property.

--- src/wrappers/TaxonNameWrapper.js
+++ src/wrappers/TaxonNameWrapper.js
@@ -88,7 +88,7 @@ class TaxonNameWrapper {
     if (results) {
       txname = {
         '@type': TaxonNameWrapper.TYPE_TAXON_NAME,
-        nomenclaturalCode: TaxonNameWrapper.getNomenCodeAsURI(nomenCode),
+        nomenclaturalCode: nomenCode,
         label: verbatimName,
         nameComplete: `${results[1]} ${results[2]} ${results[3]}`.trim(),
         genusPart: results[1],
@@ -120,7 +120,7 @@ class TaxonNameWrapper {
       if (results) {
         txname = {
           '@type': TaxonNameWrapper.TYPE_TAXON_NAME,
-          nomenclaturalCode: TaxonNameWrapper.getNomenCodeAsURI(nomenCode),
+          nomenclaturalCode: nomenCode,
           label: verbatimName,
           nameComplete: results[1],
           uninomial: results[1],
@@ -159,6 +159,13 @@ class TaxonNameWrapper {
    */
   get nomenclaturalCode() {
     return this.txname.nomenclaturalCode || TaxonNameWrapper.getNomenCodeAsURI('unknown');
+  }
+
+  /**
+   * Set the nomenclatural code of this taxon name.
+   */
+  set nomenclaturalCode(nomenCode) {
+    this.txname.nomenclaturalCode = nomenCode;
   }
 
   /**
@@ -332,7 +339,7 @@ class TaxonNameWrapper {
   /**
    * Return this taxon name in an JSON-LD representation.
    */
-  asJSONLD() {
+  get asJSONLD() {
     const jsonld = cloneDeep(this.txname);
 
     // Make sure '@type' is an array.
