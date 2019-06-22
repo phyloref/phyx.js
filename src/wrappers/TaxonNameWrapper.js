d073088df8ac3beabcdcb45c8bbf39e7c82713b9
Gaurav Vaidya <gaurav@ggvaidya.com>
2019-06-22 08:22:07 -0400

    Changed short name of ICNafp to ICN.

--- src/wrappers/TaxonNameWrapper.js
+++ src/wrappers/TaxonNameWrapper.js
@@ -78,7 +78,7 @@ class TaxonNameWrapper {
       },
       {
         uri: owlterms.ICN_NAME,
-        shortName: 'ICNafp',
+        shortName: 'ICN',
         label: 'Algae, fungi and plants (ICNafp or ICBN)',
         title: 'International Code of Nomenclature for algae, fungi, and plants',
       },
