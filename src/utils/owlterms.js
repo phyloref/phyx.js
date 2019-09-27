2e3b31316dd8f423044f4f29f839c59d699d40a4
Gaurav Vaidya <gaurav@ggvaidya.com>
2019-09-27 19:15:32 -0400

    Removed unnecessary term from owlterms.

--- src/utils/owlterms.js
+++ src/utils/owlterms.js
@@ -46,9 +46,6 @@ module.exports = {
   ICNP_NAME: 'http://purl.obolibrary.org/obo/NOMEN_0000110',
   ICTV_NAME: 'http://purl.obolibrary.org/obo/NOMEN_0000111',
 
-  // Terms from the Relation Ontology.
-  RO_HAS_EVIDENCE: 'obo:RO_0002558',
-
   // Terms from the Ontology for Biomedical Investigations.
   OBI_HAS_SPECIFIED_INPUT: 'obo:OBI_0000293',
   OBI_IS_SPECIFIED_OUTPUT_OF: 'obo:OBI_0000312',
