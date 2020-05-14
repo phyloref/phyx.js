804f27845a354de2da3a339c411c5aedf8a9a9c9
Gaurav Vaidya <gaurav@renci.org>
2020-05-14 14:58:55 -0400

    Instead of converting JSON-LD to n-quads, convert the JSON directly.

--- test/examples.js
+++ test/examples.js
@@ -18,6 +18,7 @@ const expect = chai.expect;
  */
 
 describe('PhyxWrapper', function () {
+  let brochu2003owl;
   describe('convert brochu_2003.json to an OWL ontology', function () {
     const jsonFilename = path.resolve(__dirname, './examples/brochu_2003.json');
     const jsonldFilename = path.resolve(__dirname, './examples/brochu_2003.jsonld');
@@ -29,7 +30,6 @@ describe('PhyxWrapper', function () {
       expect(brochu2003).to.be.an('object');
     });
 
-    let brochu2003owl;
     it('should be able to convert brochu_2003.json to an OWL Ontology', function () {
       this.timeout(10000);
       brochu2003owl = new phyx.PhyxWrapper(brochu2003).asOWLOntology('http://example.org/brochu_2003.json');
@@ -43,15 +43,7 @@ describe('PhyxWrapper', function () {
     });
   });
   describe('convert brochu_2003.jsonld to n-quads', function () {
-    const jsonldFilename = path.resolve(__dirname, './examples/brochu_2003.jsonld');
     const nqFilename = path.resolve(__dirname, './examples/brochu_2003.nq');
-
-    let brochu2003jsonld;
-
-    it('should be able to load brochu_2003.jsonld', function () {
-      brochu2003jsonld = JSON.parse(fs.readFileSync(jsonldFilename));
-      expect(brochu2003jsonld).to.be.an('object');
-    });
 
     let context;
     it('should be able to load the current context file (docs/context/development)', function () {
@@ -60,11 +52,11 @@ describe('PhyxWrapper', function () {
     });
 
     let brochu2003nq;
-    it('should be able to convert brochu_2003.jsonld to n-quads', function () {
+    it('should be able to convert brochu_2003.json via JSON-LD to n-quads', function () {
       this.timeout(10000);
       // Replace the @context on the fly.
-      brochu2003jsonld['@context'] = context['@context'];
-      return jsonld.toRDF(brochu2003jsonld, { format: 'application/n-quads' }).then((rdf) => {
+      brochu2003owl['@context'] = context['@context'];
+      return jsonld.toRDF(brochu2003owl, { format: 'application/n-quads' }).then((rdf) => {
         brochu2003nq = rdf;
         // fs.writeFileSync(nqFilename, brochu2003nq);
         expect(brochu2003nq).to.be.a('string');
