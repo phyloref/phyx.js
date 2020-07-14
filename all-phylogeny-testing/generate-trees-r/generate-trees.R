44b211b6fe3d3223350a5aaa0eff5a25234b1fa0
Gaurav Vaidya <gaurav@renci.org>
2020-07-14 16:20:58 -0400

    First stab at generating phylogenies in R.

---
+++ all-phylogeny-testing/generate-trees-r/generate-trees.R
@@ -0,0 +1,14 @@
+# install.packages('phytools');
+require('phytools');
+
+# Using allFurcTrees.
+LEAF_COUNT = 5;
+all_trees = allFurcTrees(LEAF_COUNT);
+write.tree(all_trees, paste("all_trees_", LEAF_COUNT, ".txt", sep=""));
+plot(all_trees);
+
+# Using multi2di.
+trees <- do.call(c, lapply(1:1000, function(x) multi2di(starTree(LETTERS[1:LEAF_COUNT]))))
+uniqueTrees <- unique.multiPhylo(trees)
+length(uniqueTrees);
+write.tree(uniqueTrees, paste("all_trees_", LEAF_COUNT, ".txt", sep=""));
