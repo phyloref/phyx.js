f80f5ac0ef117b8694226c852d904d5bbd946e05
Gaurav Vaidya <gaurav@ggvaidya.com>
2021-02-09 15:54:32 -0500

    Rewrote examples to handle any number of example files.

--- test/examples.js
+++ test/examples.js
@@ -5,7 +5,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const jsonld = require('jsonld');
+const JSONLD = require('jsonld');
 const chai = require('chai');
 const Ajv = require('ajv');
 
@@ -26,84 +26,80 @@ const REPLACE_EXISTING = false;
  */
 
 describe('PhyxWrapper', function () {
-  let brochu2003owl;
-  describe('convert brochu_2003.json to an OWL ontology', function () {
-    const jsonFilename = path.resolve(__dirname, './examples/correct/brochu_2003.json');
-    const jsonldFilename = path.resolve(__dirname, './examples/correct/brochu_2003.jsonld');
-
-    let brochu2003;
-
-    it('should be able to load brochu_2003.json', function () {
-      brochu2003 = JSON.parse(fs.readFileSync(jsonFilename));
-      expect(brochu2003).to.be.an('object');
-    });
-
-    it('should be able to convert brochu_2003.json to an OWL Ontology', function () {
-      this.timeout(10000);
-      brochu2003owl = new phyx.PhyxWrapper(brochu2003).asOWLOntology('http://example.org/brochu_2003.json#');
-      if (REPLACE_EXISTING) {
-        fs.writeFileSync(
-          jsonldFilename,
-          JSON.stringify(brochu2003owl, null, 2)
-        );
-      }
-      expect(brochu2003owl).to.be.an('object');
-    });
-
-    it('should generate the same OWL ontology as it generated earlier', function () {
-      const expectedBrochu2003 = JSON.parse(fs.readFileSync(jsonldFilename));
-      expect(brochu2003owl).to.be.deep.equal(expectedBrochu2003);
-    });
+  const ajv = new Ajv({
+    allErrors: true, // Display all error messages, not just the first.
   });
-  describe('convert brochu_2003.jsonld to n-quads', function () {
-    const nqFilename = path.resolve(__dirname, './examples/correct/brochu_2003.nq');
-
-    let brochu2003nq;
-    it('should be able to convert brochu_2003.json via JSON-LD to n-quads', function () {
-      this.timeout(10000);
-
-      // JSON-LD readers don't usually handle relative @context easily, so
-      // instead let's replace the entire @context with the local context file.
-      brochu2003owl['@context'] = JSON.parse(fs.readFileSync(
-        path.resolve(__dirname, path.join('examples', 'correct', brochu2003owl['@context']))
-      ));
-
-      return jsonld.toRDF(brochu2003owl, { format: 'application/n-quads' }).then((rdf) => {
-        brochu2003nq = rdf;
-        if (REPLACE_EXISTING) fs.writeFileSync(nqFilename, brochu2003nq);
-        expect(brochu2003nq).to.be.a('string');
-      });
-    });
-
-    it('should generate the same n-quads ontology as it generated earlier', function () {
-      const expectedBrochu2003nq = fs.readFileSync(nqFilename).toString();
-      expect(brochu2003nq).to.be.deep.equal(expectedBrochu2003nq);
-    });
-  });
+  const validator = ajv.compile(
+    JSON.parse(
+      fs.readFileSync(
+        path.resolve(__dirname, '../docs/context/development/schema.json')
+      )
+    )
+  );
 
   describe('Test all example Phyx files', function () {
-    const examples = fs.readdirSync(path.resolve(__dirname, './examples'));
-    const jsonlds = examples.filter(filename => filename.toLowerCase().endsWith('.json'));
-    const ajv = new Ajv();
-    const validator = ajv.compile(
-      JSON.parse(
-        fs.readFileSync(
-          path.resolve(__dirname, '../docs/context/development/schema.json')
-        )
-      )
-    );
+    const examples = fs.readdirSync(path.resolve(__dirname, './examples/correct'))
+      .filter(filename => filename.endsWith('.json'));
 
-    jsonlds.forEach((filename) => {
-      describe(`Example file ${filename}`, function () {
+    examples.forEach((example) => {
+      const basename = path.resolve(__dirname, './examples/correct', path.parse(example).name);
+      const jsonFilename = `${basename}.json`;
+      const jsonldFilename = `${basename}.jsonld`;
+      const nqFilename = `${basename}.nq`;
+
+      let json;
+      let jsonld;
+      let nq;
+
+      describe(`Test file '${example}'`, function () {
+        it('should be loadable', function () {
+          json = JSON.parse(fs.readFileSync(jsonFilename));
+          expect(json).to.be.an('object');
+        });
+
         it('should validate against our JSON schema', function () {
-          const phyxContent = JSON.parse(
-            fs.readFileSync(
-              path.resolve(__dirname, `./examples/correct/${filename}`)
-            )
-          );
-          const valid = validator(phyxContent);
+          const valid = validator(json);
           expect(validator.errors).to.be.null;
           expect(valid).to.be.true;
+        });
+
+        it('should be able to convertible to an OWL Ontology', function () {
+          this.timeout(10000);
+          jsonld = new phyx.PhyxWrapper(json)
+            .asOWLOntology('http://example.org/examples#');
+          if (REPLACE_EXISTING) {
+            fs.writeFileSync(
+              jsonldFilename,
+              JSON.stringify(jsonld, null, 2)
+            );
+          }
+          expect(jsonld).to.be.an('object');
+        });
+
+        it('should generate the same OWL ontology as it generated earlier', function () {
+          const expectedJSONLD = JSON.parse(fs.readFileSync(jsonldFilename));
+          expect(jsonld).to.deep.equal(expectedJSONLD);
+        });
+
+        it('should be convertible to n-quads', function () {
+          this.timeout(10000);
+
+          // JSON-LD readers don't usually handle relative @context easily, so
+          // instead let's replace the entire @context with the local context file.
+          jsonld['@context'] = JSON.parse(fs.readFileSync(
+            path.resolve(__dirname, path.join('examples', 'correct', jsonld['@context']))
+          ));
+
+          return JSONLD.toRDF(jsonld, { format: 'application/n-quads' }).then((rdf) => {
+            nq = rdf;
+            if (REPLACE_EXISTING) fs.writeFileSync(nqFilename, nq);
+            expect(nq).to.be.a('string');
+          });
+        });
+
+        it('should generate the same n-quads ontology as it generated earlier', function () {
+          const expectedNQ = fs.readFileSync(nqFilename).toString();
+          expect(nq).to.deep.equal(expectedNQ);
         });
       });
     });
